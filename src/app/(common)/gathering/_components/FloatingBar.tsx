be8bb774eba9d6b215d937b30822df89155a002d
dohyeons <ddhhss0603@gmail.com>
2025-03-14 09:43:13 +0900

    FloatingBar에 utc추가

--- src/app/(common)/gathering/_components/FloatingBar.tsx
+++ src/app/(common)/gathering/_components/FloatingBar.tsx
@@ -11,8 +11,10 @@ import { useLeaveGathering } from "@/hooks/useLeaveGathering";
 import { useCancelGathering } from "../_hooks/useCancelGathering";
 import type { GatheringType } from "@/types";
 import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 
+dayjs.extend(utc);
 dayjs.extend(timezone);
 type FloatingBarProps = {
   gatheringId: string;
